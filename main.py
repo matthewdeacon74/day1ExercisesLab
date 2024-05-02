c89948aeaac073b8d6aea81a4564d5d65c588d9c
Matthew Deacon <matthewdeacon74@gmail.com>
2024-05-01 21:22:07 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# exercise 1: echo string
+user_input = input('Enter some text: ')
+print(user_input)
+
+# exercise 2: adding number to an integer
+user_int = int(input('Enter a number: '))
+print(user_int + 1)
+
+# exercise 3: adding a number to a float
+user_float = float(input('Enter a number: '))
+print(user_float + 0.5)
+
+#exercise 4: adding 2 floats
+user_float4a = float(input('Enter a number: '))
+user_float4b = float(input('Enter another number: '))
+print(user_float4a + user_float4b)
+
+# Exercise 5: multiplying floats
+user_float5a = float(input('Enter a number: '))
+user_float5b = float(input('Enter another number: '))
+print(user_float5a * user_float5b)
+
+# Exercise 6: dividing integers
+user_int6a = int(input('Enter a number: '))
+user_int6b = int(input('Enter another number: '))
+print(user_int6a / user_int6b)
